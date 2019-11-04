14e3665b4dcc163a28e6ce891a46b783934d03fe
turnip <machinehum@wlkr.wlker>
2019-11-03 20:36:11 -0800

    Fixed up the init script

--- NN/audio/init.sh
+++ NN/audio/init.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-# wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
-# Install conda
+# Install anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh 
+chmod +x Anaconda3-2019.10-Linux-x86_64.sh
+./Anaconda3-2019.10-Linux-x86_64.sh
 
-# sudo apt install python3 
-# sudo apt install python3-pip 
-# sudo apt install python3-tk
-
-pip install numpy
-pip install pandas
-pip install seaborn
-pip install matplotlib
-pip install tensorflow
-pip install keras
-pip install jupyter
-pip install tqdm
-pip install sklearn 
-# pip install tensorflow-gpu
-# pip install tf-nightly
+source ~/.bashrc                        # Load in the new bashrc
+conda env create --file environment.yml # Create the new env
+conda activate birdear                  # Activate the env

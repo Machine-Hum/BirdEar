9a6159c4bd5fd322d3951582a38db811f7dc045e
turnip <machinehum@wlkr.wlker>
2019-11-05 21:41:09 -0800

    Created a fetcher script

---
+++ NN/data/fetch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+Name='Carrion+crow' # Bird Name
+Page=1 # Start at the first page
+
+wget "https://www.xeno-canto.org/explore?query=${Name}&pg=${Page}"
+
+fname="explore?query=${Name}&pg=${Page}"
+cat ${fname} | grep download | grep -o -P "href.*download'" | tr -d "href='" > out
+rm ${fname}
+
+mkdir $Page
+mv out $Page
+cd $Page
+
+for i in {1..30}
+do
+  dl="$(sed -n "${i}p" out)"
+  echo $dl
+  wget "https://www.xeno-canto.org${dl}"
+done
